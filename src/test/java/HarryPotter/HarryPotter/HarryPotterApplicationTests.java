055cce1b77f4e1d37992d38776784774a105c678
lislenytorquato <lislenytorquato19@gmail.com>
2025-06-27 08:49:35 -0300

    Adicionando projeto

---
+++ src/test/java/HarryPotter/HarryPotter/HarryPotterApplicationTests.java
@@ -0,0 +1,13 @@
+package HarryPotter.HarryPotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarryPotterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
