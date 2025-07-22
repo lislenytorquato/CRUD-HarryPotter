5a13139bc633fc3176f4b15f5aafdff2c9279144
lislenytorquato <lislenytorquato19@gmail.com>
2025-07-22 14:27:52 -0300

    Refactor: refactoring BruxoServiceTest and BruxoControllerTest

--- src/main/java/HarryPotter/HarryPotter/controller/BruxoController.java
+++ src/main/java/HarryPotter/HarryPotter/controller/BruxoController.java
@@ -2,7 +2,6 @@ package HarryPotter.HarryPotter.controller;
 
 import HarryPotter.HarryPotter.dto.BruxoRequestDto;
 import HarryPotter.HarryPotter.dto.BruxoResponseDto;
-import HarryPotter.HarryPotter.enums.CasaEnum;
 import HarryPotter.HarryPotter.service.BruxoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,44 +13,44 @@ import java.util.List;
 @RequestMapping("/bruxo")
 public class BruxoController {
 
-    BruxoService service;
+    private final BruxoService bruxoService;
 
     public BruxoController(BruxoService bruxoService){
-        this.service = bruxoService;
+        this.bruxoService = bruxoService;
     }
 
     @PostMapping
     public ResponseEntity<BruxoResponseDto> criar(@RequestBody BruxoRequestDto bruxoRequestDto) {
-        BruxoResponseDto bruxoResponseDto = service.criarBruxo(bruxoRequestDto);
+        BruxoResponseDto bruxoResponseDto = bruxoService.criarBruxo(bruxoRequestDto);
         return new ResponseEntity<>(bruxoResponseDto, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<BruxoResponseDto>> listar(){
-        List<BruxoResponseDto> listaBruxoResponseDto = service.listarBruxos();
+        List<BruxoResponseDto> listaBruxoResponseDto = bruxoService.listarBruxos();
         return new ResponseEntity<>(listaBruxoResponseDto,HttpStatus.OK);
     }
 
     @PutMapping("/{idBruxo}")
     public ResponseEntity<BruxoResponseDto> atualizar(@PathVariable Long idBruxo, @RequestBody BruxoRequestDto bruxoRequestDto) {
-        BruxoResponseDto bruxoResponseDto = service.atualizarBruxo(idBruxo,bruxoRequestDto);
+        BruxoResponseDto bruxoResponseDto = bruxoService.atualizarBruxo(idBruxo,bruxoRequestDto);
         return new ResponseEntity<>(bruxoResponseDto,HttpStatus.OK);
     }
     @DeleteMapping("/{idBruxo}")
     public ResponseEntity<Void> deletar(@PathVariable Long idBruxo){
-        service.deletarBruxo(idBruxo);
+        bruxoService.deletarBruxo(idBruxo);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/mostrar-informacoes/{idBruxo}")
     public ResponseEntity<String> mostrarInformacoes(@PathVariable Long idBruxo) {
-        String informacoes = service.mostrarInformacoes(idBruxo);
+        String informacoes = bruxoService.mostrarInformacoes(idBruxo);
         return new ResponseEntity<>(informacoes,HttpStatus.OK);
     }
 
     @GetMapping("/lancar-feitico/{idBruxo}")
     public ResponseEntity<String> lancarFeitico(@PathVariable Long idBruxo) {
-        String feiticoLancado = service.lancaFeitico(idBruxo);
+        String feiticoLancado = bruxoService.lancaFeitico(idBruxo);
         return new ResponseEntity<>(feiticoLancado,HttpStatus.OK);
     }
 
